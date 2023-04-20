c6768dda34b365cb9970e26330edbc1316b4db36
Brandon Knight <knightbrandon1984@gmail.com>
2023-04-20 14:08:58 -0500

    redirecting request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const http = require('http'); 
+const http = require('http');
+const fs = require('fs');
 
 // function rqListener(req, res) {
 
@@ -6,6 +7,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
@@ -15,6 +17,12 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
