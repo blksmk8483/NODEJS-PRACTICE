bc12c730f245cf2f66cbca0d6905a5552e343502
Brandon Knight <knightbrandon1984@gmail.com>
2023-04-25 22:42:02 -0500

    appListen

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const express = require('express');
 const app = express();
 
 //middleware
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
-
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('Another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
+app.use('/', (req, res, next) => {
+    console.log('Another middleware!');
+    res.send('<h1>Hello from the other side!</h1>');
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000);
