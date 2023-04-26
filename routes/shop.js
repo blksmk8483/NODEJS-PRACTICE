60a9d7c84fe354ca67889117bce8f60857200309
Brandon Knight <knightbrandon1984@gmail.com>
2023-04-26 15:55:44 -0500

    cleaned up console.logs

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('Another middleware!');
     res.send('<h1>Hello from the other side!</h1>');
 });
 
