a246aea2dd73987f7a5a92be5310b7470a423348
Brandon Knight <knightbrandon1984@gmail.com>
2023-04-26 14:59:13 -0500

    more middleware practice

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 //middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+// This middleware will always run
+app.use('/', (req, res, next) => {
+    console.log('This will always run!!!');
+    next();
+});
+
 app.use('/add-product', (req, res, next) => {
     console.log('Another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.use('/', (req, res, next) => {
     console.log('Another middleware!');
     res.send('<h1>Hello from the other side!</h1>');
