029ecafc6990c9d84ad28df112770bd91236d4f5
Brandon Knight <knightbrandon1984@gmail.com>
2023-04-26 22:10:46 -0500

    added views folder with add-product.html, shop.html

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-
+// localhost:3000/admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+// localhost:3000/admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
