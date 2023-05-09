669a4b1ea0c1d55dd8368172de01046a9ee0fecb
Brandon Knight <knightbrandon1984@gmail.com>
2023-05-08 22:37:47 -0500

    converting my html templates into pug templates for learning

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 
 // localhost:3000/admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Prodcut' })
 });
 
 // localhost:3000/admin/add-product => POST
