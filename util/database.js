536ca30176ccb83b387db0179dd83a598af17da3
Brandon Knight <knightbrandon1984@gmail.com>
2023-06-29 01:00:43 -0500

    fetching products

--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   database: "node-complete",
   password: "1Drummer@1",
+  port: "3306",
 });
 
 module.exports = pool.promise();
