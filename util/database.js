005f719f83ec9c0c615347667b631cc7dc58cd7f
Brandon Knight <knightbrandon1984@gmail.com>
2023-07-01 14:26:36 -0500

    connecting the db using sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  database: "node-complete",
-  password: "1Drummer@1",
-  port: "3306",
+const sequelize = new Sequelize("node-complete", "root", "1Drummer@1", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
